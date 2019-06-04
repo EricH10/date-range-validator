0a5e8f5b685a6c5e1f84a5a44c1bcdbbef085007
Eric <ehenderson5225@gmailc.om>
2019-06-03 20:24:19 -0400

    unblocked blocking code

--- src/main/scala/com/campspot/Routes.scala
+++ src/main/scala/com/campspot/Routes.scala
@@ -1,16 +1,16 @@
 package com.campspot
 
-import akka.actor.{ ActorRef, ActorSystem, Status }
+import akka.actor.{ ActorRef, ActorSystem }
 import akka.event.Logging
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
-import akka.pattern.{ ask, pipe }
+import akka.pattern.{ ask }
 
-import scala.concurrent.{ Await, Future }
+import scala.concurrent.{ Future }
 import akka.util.Timeout
 import com.campspot.actors.FileParserActor.ParseFile
 import com.campspot.actors.SearchActor.SearchObject
@@ -36,13 +36,11 @@ trait Routes extends JsonSupport {
               parameters('filename.as[String]) { (filename) =>
                 val searchObject: Future[Option[SearchObject]] =
                   (fileParserActor ? ParseFile(filename)).mapTo[Option[SearchObject]]
-                //blocking code can resolve this a few different ways
-                val search = Await.result(searchObject, 2 second)
 
-                val campsites: Future[Campsites] =
-                  (searchActor ? search).mapTo[Campsites]
-
-                complete(campsites)
+                val result = searchObject map { searchObject =>
+                  (searchActor ? searchObject).mapTo[Campsites]
+                }
+                complete(result)
               }
             })
         })
