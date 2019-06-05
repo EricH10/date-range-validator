146e33e436f1146fc694b7536854f6ee0564e086
Eric <ehenderson5225@gmailc.om>
2019-06-04 20:15:02 -0400

    cleaning up comments

--- src/test/scala/com/example/RoutesSpec.scala
+++ src/test/scala/com/example/RoutesSpec.scala
@@ -1,7 +1,5 @@
 package com.example
 
-//#user-routes-spec
-//#test-top
 import akka.actor.{ ActorRef, Props }
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
@@ -10,10 +8,8 @@ import com.campspot.{ Campsites, Routes }
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{ Matchers, WordSpec }
 
-//#set-up
 class RoutesSpec extends WordSpec with Matchers with ScalaFutures with ScalatestRouteTest
   with Routes {
-  //#test-top
 
   // Here we need to implement all the abstract members of Routes.
   // We use the real SearchActor to test it while we hit the Routes,
@@ -26,28 +22,23 @@ class RoutesSpec extends WordSpec with Matchers with ScalaFutures with Scalatest
 
   lazy val routes = userRoutes
 
-  //#set-up
-
-  //#actual-test
   "Routes" should {
     "return 3 campsites if present (GET /search)" in {
-      // note that there's no need for the host part in the uri:
+
       val request = HttpRequest(uri = "/search?filename=test-case.json")
 
       request ~> routes ~> check {
         status should ===(StatusCodes.OK)
 
-        // we expect the response to be json:
         contentType should ===(ContentTypes.`application/json`)
 
-        // and no entries should be in the list:
         entityAs[Campsites].campsites.size should ===(3)
       }
     }
   }
 
   "return 5 campsites if present (GET /search)" in {
-    // note that there's no need for the host part in the uri:
+
     val request = HttpRequest(uri = "/search?filename=test-case-partially-empty.json")
 
     request ~> routes ~> check {
@@ -56,13 +47,12 @@ class RoutesSpec extends WordSpec with Matchers with ScalaFutures with Scalatest
       // we expect the response to be json:
       contentType should ===(ContentTypes.`application/json`)
 
-      // and no entries should be in the list:
       entityAs[Campsites].campsites.size should ===(5)
     }
   }
 
   "return no campsites if no file exists (GET /search)" in {
-    // note that there's no need for the host part in the uri:
+
     val request = HttpRequest(uri = "/search?filename=doesnt-exist")
 
     request ~> routes ~> check {
@@ -71,7 +61,6 @@ class RoutesSpec extends WordSpec with Matchers with ScalaFutures with Scalatest
       // we expect the response to be json:
       contentType should ===(ContentTypes.`application/json`)
 
-      // and no entries should be in the list:
       entityAs[Campsites].campsites.size should ===(0)
     }
   }
